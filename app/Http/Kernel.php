76a690492395b9d2ccf01c20a7e7ac405a0c34f8
Sven Baerten <sven.baerten@outlook.com>
2019-05-30 20:52:06 +0200

    test

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,12 +38,14 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             // \Barryvdh\Cors\HandleCors::class, // https://github.com/barryvdh/laravel-cors
+            \Spatie\Cors\Cors::class, // https://freek.dev/handling-cors-in-a-laravel-application
         ],
 
         'api' => [
             'throttle:60,1',
             'bindings',
             // \Barryvdh\Cors\HandleCors::class, // https://github.com/barryvdh/laravel-cors
+            \Spatie\Cors\Cors::class, // https://freek.dev/handling-cors-in-a-laravel-application
         ],
     ];
 
@@ -65,7 +67,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         // 'cors' => \Barryvdh\Cors\HandleCors::class // https://www.codeinhouse.com/is-laravel-cors-not-working-how-to-fix-access-control-allow-origin-problem-solution/
-        'cors' => \Spatie\Cors\Cors::class, // https://freek.dev/handling-cors-in-a-laravel-application,
+        // 'cors' => \Spatie\Cors\Cors::class, // https://freek.dev/handling-cors-in-a-laravel-application,
     ];
 
     /**
